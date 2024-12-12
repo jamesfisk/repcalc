4fbc49415911a376750a20a805e545128803ec57
James Fisk <james.fisk@discordapp.com>
2024-12-12 18:47:31 -0500

    escape chars

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <br />
             <p>The <a href='/loading' className="text-sky-600">loading guide</a> shows the plates needed on each side for an estimated load in kg or lbs. It assumes a 20kg/45lbs bar. You can view kg loading with or without 2.5kg collars.</p>
             <br />
-            <p>The <a href="/platecalculator" className="text-sky-600">barbell weight calculator</a> shows the total weight of a barbell from the plates you've load onto it. It only counts plates from a single side and assumes symmetrical loading. For example, 2 45lbs plates would result in 225 lbs total: 2 plates on each side plus a 45 lbs bar.</p>
+            <p>The <a href="/platecalculator" className="text-sky-600">barbell weight calculator</a> shows the total weight of a barbell from the plates you&apos;ve loaded onto it. It only counts plates from a single side and assumes symmetrical loading. For example, 2 45lbs plates would result in 225 lbs total: 2 plates on each side plus a 45 lbs bar.</p>
             <br />
             <p>The DOTS score calculator uses the formula from <a className='text-blue-500 dark:text-blue-300' target="_blank" href="https://www.powerlifting.sport/rules/codes/info/ipf-formula">IPF regulations</a>.</p>
             <br />
@@ -33,7 +33,7 @@ export default function Home() {
             <br />
             <p>Have feedback? <a className='text-blue-500 dark:text-blue-300' target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdZgCf4Tu6y-5SvOfZkanyqr0pQj8M9u7-RpRiWFUvL4piTQA/viewform?usp=sf_link">Help us improve</a>!</p>
             <br />
-            <p>Let's go lift 🏋️‍♂️</p>
+            <p>Let&apos;s go lift 🏋️‍♂️</p>
           </div>
           <div className='py-8 text-xs w-full text-center'>Made with 💪 by <a className='text-blue-500 dark:text-blue-300' href="https://www.jamesfisk.io" target="_blank">james</a></div>
         </div>
